e51420be13b10333fc10272d3772ac424c1ee1aa
Fabrice LATRI <fabrice.latri@carbon-it.com>
2023-12-03 00:22:07 +0100

    Fix error build

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,6 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  semi: true,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
